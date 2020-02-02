bfb5c3d25319da20e82b1a4ecdc69091b651fb68
selinahuang20304 <60204684+selinahuang20304@users.noreply.github.com>
2020-02-02 06:54:14 -0500

    dropphone feature added
    Phone Feature

---
+++ Assets/GravityOn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityOn : MonoBehaviour
+{
+    /*public Rigidbody rb; //= GetComponent<Rigidbody>(); 
+    float gravity = -9.8f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    } */
+    Animator anim; 
+    void Start(){
+        anim = GetComponent<Animator>(); 
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("PhoneDrop");
+            //GetComponent<Rigidbody>().useGravity = true;
+            //GetComponent<Rigidbody>() = new Vector3(rb.velocity.x, rb.velocity.y +gravity * Time.deltaTime,rb.velocity.z );
+            //GetComponent<Rigidbody>().isKinematic = false;
+            //Debug.Log("hello");
+        }
+    }
+}
